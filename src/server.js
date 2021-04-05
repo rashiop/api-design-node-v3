e04000a52cdb3d54a1fc30bf5af7d68f0bb19469
rashiop <red.alpacca@gmail.com>
2021-04-06 01:11:34 +0700

    lesson 1

--- src/server.js
+++ src/server.js
@@ -1,15 +1,34 @@
-import express from 'express'
-import { json, urlencoded } from 'body-parser'
-import morgan from 'morgan'
-import cors from 'cors'
+import { json, urlencoded } from 'body-parser';
+import cors from 'cors';
+import express from 'express';
+import morgan from 'morgan';
 
-export const app = express()
+export const app = express();
+const router = express.Router();
 
-app.disable('x-powered-by')
+app.disable('x-powered-by');
 
-app.use(cors())
-app.use(json())
-app.use(urlencoded({ extended: true }))
-app.use(morgan('dev'))
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
-export const start = () => {}
+router.get('/me', (req, res, next) => {
+  res.send({ me: 'hello' });
+});
+
+app.use('/api', router);
+
+app.get('/data', (req, res) => {
+  res.send({ message: 'hello2' });
+});
+
+app.post('/data', (req, res) => {
+  res.send(req.body);
+});
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('server is on 3000');
+  });
+};
